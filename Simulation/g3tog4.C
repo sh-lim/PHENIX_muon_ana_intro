5aa32526f16829fdd6337046cd81dc64038ae6d4
Sanghoon Lim <shlim@rcf.rhic.bnl.gov>
2019-08-31 07:19:19 -0400

    G4 simulation macros, initial commit

---
+++ Simulation/g3tog4.C
@@ -0,0 +1,40 @@
+void g3tog4(
+	    int nevents = 0, 
+	    std::string inputname = "pythia_mn.root"
+	    )
+{
+
+  gROOT->LoadMacro("g4libs.C");
+  g4libs();
+
+  gSystem->Load("libfun4all.so");	// framework + reco modules
+  gSystem->Load("libfvtx_subsysreco.so");
+  gSystem->Load("libsimreco.so");
+  gSystem->Load("libPHHepMCNode.so");
+  gSystem->Load("/gpfs/mnt/gpfs02/phenix/hhj/hhj1/shlim/work/PHENIX_muon_ana_intro/Simulation/PHG3toG4/install/lib/libPHG3toG4.so");
+
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+  
+  SubsysReco *sync = new SyncSimreco();
+  se->registerSubsystem(sync);
+
+	PHG3toG4 *myG3toG4 = new PHG3toG4("PHG3toG4","Sim3D++.root","geom_run15.root");
+  myG3toG4->AddNode("PHHepMCGenEvent_SIGNAL");
+  se->registerSubsystem(myG3toG4);
+
+	myG3toG4->ReportEvery(1);
+
+  // DST output manager
+  Fun4AllDstInputManager *dst_input_mgr  = new Fun4AllDstInputManager("HepMC","DST");
+  se->registerInputManager(dst_input_mgr);
+  se->fileopen( dst_input_mgr->Name(), inputname.c_str() );
+
+	gSystem->ListLibraries();
+
+  // run over all events
+  se->run(nevents);  
+  se->End();
+
+}
+
